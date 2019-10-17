2088dba3fa685cddf1b05a3f7fedaeee70e1ed93
unknown <mallikarjunchv@gmail.com>
2019-10-17 10:23:09 +0530

    dashborad page

--- src/pages/ProfileSummary.js
+++ src/pages/ProfileSummary.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Card,Button } from 'react-bootstrap';
+import {Card,Button,Image} from 'react-bootstrap';
 import '../css/ProfileSummary.css';
-import { FaVideo ,FaBullhorn,FaWikipediaW} from 'react-icons/fa';
+import { FaVideo ,FaBullhorn,FaWikipediaW,FaEnvelope,FaPhone,FaRocketchat} from 'react-icons/fa';
 
 const cardstyle={
     background: "white",
@@ -14,7 +14,7 @@ const ProfileSummary = () => {
          <div className ="profile">
        <Card>
   <Card.Header style={cardstyle}>
-     <div className="col-md-9"><p>   CV-Summarization Chart </p></div>
+     <div className="col-md-9"><p className="head_con">   CV-Summarization Chart </p></div>
      <div className="col-md-3">
        <div className="d-flex justify-content-around">
         <div><p className="resume">Resume</p> </div>
@@ -26,18 +26,45 @@ const ProfileSummary = () => {
      </div>
        </div>
        </div>
-    {/* <div className="col-md-1">Resume </div>
-    <div className="col-md-1"><FaVideo/></div>
-    <div className="col-md-1"> <FaBullhorn/></div>
-    <div className="col-md-1"> <FaWikipediaW/></div>
-    <div className="col-md-1"> Interview </div> */}
      </Card.Header>
-  <Card.Body>
-    <Card.Title>Special title treatment</Card.Title>
-    <Card.Text>
-      With supporting text below as a natural lead-in to additional content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+  <Card.Body style={cardstyle}>
+  <div className="col-md-3">
+    <div className="d-flex justify-content-around">
+    <div>
+  <Image src="../profile1.png" roundedCircle className="img_pro" />
+  </div>
+  <div className="Pro_con">
+<p className="UsreName">Sharon Mathews </p>
+<p>Senior java Programmer @infosys</p>
+  <div className="d-flex"> 
+  <div eventKey={4}  className="icon px-2"> <FaPhone/></div> 
+  <div eventKey={4}  className="icon px-2"> <FaEnvelope/></div> 
+  <div eventKey={4}  className="icon px-2"> <FaRocketchat/></div> 
+  </div>
+  </div>
+  </div>
+  </div>
+  <div className="col-md-9">
+    <div className="d-flex  justify-content-around align" >
+<div className="User_con"> <p>Status</p>
+ <p className="login">Logged in</p></div>
+<div className="User_con"> <p>Visa:</p>
+ <p className="_con">H1B</p></div>
+<div className="User_con"> <p>Employed</p>
+ <p className="agency">Agency</p></div>
+<div className="User_con"> <p>Rank</p>
+ <p className="_con">100%</p></div>
+<div className="User_con"><p>Interview</p>
+ <p className="_con">25</p></div>
+<div className="User_con"><p>Qualified</p>
+ <p className="login">20</p></div>
+<div className="User_con"> <p>Rejected</p>
+ <p className="reject">05</p></div>
+<div ><p className="exp_con">26</p>
+ <p className="yr">YRS EXP</p></div>
+
+</div>
+  </div>
   </Card.Body>
 </Card>
 </div>  
