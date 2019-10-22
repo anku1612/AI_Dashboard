8f0987ab94a7556f717671329f239bfbb77d42fb
unknown <mallikarjunchv@gmail.com>
2019-10-22 18:16:46 +0530

    finally all pages are done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Dashboard from './pages/Dashboard';
 import CandidateSearch from './pages/CandidateSearch';
 import RequireterMainPage from './pages/RequireterMainPage';
 import candidate_complete_profile from './pages/candidate_complete_profile';
+import PanellistDashboard from './pages/PanellistDashboard'
+import RecruiterIntelligentSearch from './pages/RecruiterIntelligentSearch'
+import HiredcandidateDashboard from './pages/HiredcandidateDashboard'
 
 import './App.css';
 
@@ -18,6 +21,9 @@ function App(){
       <Route path="/Dashboard"  component={Dashboard} />
       <Route path="/RequireterMainPage"  component={RequireterMainPage} />
       <Route path="/candidate_complete_profile"  component={candidate_complete_profile} />
+      <Route path="/panelList" component={PanellistDashboard} />
+      <Route path="/recruiter" component={RecruiterIntelligentSearch} />
+      <Route path="/hiredcandidates" component={HiredcandidateDashboard} />
     </Switch>
  </div>
     </BrowserRouter>
